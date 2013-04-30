74f89dd4810c10330abf88e049ce942d76321b70
jude <judebpereira@gmail.com>
2013-04-30 11:16:16 +0530

    Updated module description for ant

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,17 @@
 # Class: ant
 #
-# This module manages ant
+# The Apache Ant module allows Puppet to install ant and update the bashrc file to include ant in the path.
 #
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
+# Parameters: ensure, source, deploymentdir, user, pathfile
 #
 # Sample Usage:
-#
+#    ant::setup { "example.com-ant":
+#      ensure        => 'present',
+#      source        => 'apache-ant-1.9.0-bin.tar.gz',
+#      deploymentdir => '/home/example.com/apps/apache-ant',
+#      user          => 'example.com',
+#      pathfile      => '/home/example.com/.bashrc'
+#    }
 class ant {
 
 }
